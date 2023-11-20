7920feee07fadf52249d5e1bc29d4890be7cd484
Arjan Geertsema <arjan@synion.nl>
2023-11-20 09:32:37 +0100

    wip

--- src/business-reference-architecture/component.ts
+++ src/business-reference-architecture/component.ts
@@ -1,29 +1,16 @@
 import { html, css, LitElement } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { customElement } from 'lit/decorators.js';
 
 import resetStyles from '../general/styles/reset';
-import { Model } from './modules/models/model';
 
 @customElement('business-reference-architecture')
 export class BusinessReferenceArchitectureComponent extends LitElement {
-  @property() 
-  model!: Model
 
-  @property({ attribute: "model-json" })
-  modelJson!: string
-  
   override render() {
-    return html`<h1>Hello World!</h1>`;
+    return html`
+      <h1>Hello World!</h1>
+    `;
   }
-
-  override update(changedProperties: Map<string, unknown>) {
-    if(changedProperties.has("modelJson")) {
-      this.model = JSON.parse(this.modelJson);
-    }
-    super.update(changedProperties);
-  }
-
-  override updated() { }
 
   static override get styles() {
     const styles = css``;
--- src/business-reference-architecture/modules/models/model.ts
+++
@@ -1,7 +0,0 @@
-export type Model = {
-    endUsers: string[],
-    channels: string[],
-    valueStreams: string[],
-    businessCapabilities: string[],
-    principles: string
-}
