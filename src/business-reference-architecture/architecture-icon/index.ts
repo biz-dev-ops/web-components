eb7a74f983f5854ed5eb2690083710fadff3b701
Arjan Geertsema <arjan@synion.nl>
2024-01-11 13:07:04 +0100

    added multi font icon support

--- src/business-reference-architecture/architecture-icon/index.ts
+++ src/business-reference-architecture/architecture-icon/index.ts
@@ -1,6 +1,14 @@
 import { html, css, LitElement } from "lit";
 import { customElement, property } from "lit/decorators.js";
 
+/*
+Font icon support for:
+- https://fonts.google.com/icons
+- https://fontawesome.com/
+- https://phosphoricons.com/
+
+Add font files to html to enable the functionality.
+*/
 @customElement("architecture-icon")
 export class ArchitectureIcon extends LitElement {
     @property() 
@@ -17,6 +25,14 @@ export class ArchitectureIcon extends LitElement {
 
         if(this.icon.startsWith("ph-")) {
             return html`<i .className=${this.icon}></i>`;
+        }
+
+        if(this.icon.startsWith("fa-")) {
+            return html`<i class="fa-regular ${this.icon}"></i>`;
+        }
+
+        if(this.icon.startsWith("mat-")) {
+            return html`<span class="material-symbols-outlined">${this.icon.substring(4)}</span>`;
         }
 
         return html`<img .src=${this.icon} />`
