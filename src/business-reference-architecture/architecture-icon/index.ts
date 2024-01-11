490c05d67ef819e44ad9369b3e247ed79fdf448a
Arjan Geertsema <arjan@synion.nl>
2024-01-11 12:44:54 +0100

    changed icon

--- src/business-reference-architecture/architecture-icon/index.ts
+++ src/business-reference-architecture/architecture-icon/index.ts
@@ -1,27 +1,25 @@
 import { html, css, LitElement } from "lit";
 import { customElement, property } from "lit/decorators.js";
-import { unsafeHTML } from "lit/directives/unsafe-html.js";
 
 @customElement("architecture-icon")
 export class ArchitectureIcon extends LitElement {
     @property() 
     icon!: string | undefined;
-    
-    @property() 
-    svgContent!: string | undefined;
 
     override async connectedCallback() {
         super.connectedCallback();
-        this.svgContent = await this.fetchSVGContent();
-    }
-
-    async fetchSVGContent() {
-        const response = await fetch(`/assets/icons/${this.icon}.svg`);
-        return await response.text();
     }
 
     override render() {
-        return html`${unsafeHTML(this.svgContent)}`;
+        if(!this.icon) {
+            return html``;
+        }
+
+        if(this.icon.startsWith("ph-")) {
+            return html`<i .className=${this.icon}></i>`;
+        }
+
+        return html`<img .src=${this.icon} />`
     }
 
     static override get styles() {
