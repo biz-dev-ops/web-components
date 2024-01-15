56d5fd3b3cc9a689d82e6551e73a76165c166c30
sanderjn <sander@sanderjanssen.nl>
2024-01-15 17:51:31 +0100

    Add phosphor icons

--- src/business-reference-architecture/architecture-icon/index.ts
+++ src/business-reference-architecture/architecture-icon/index.ts
@@ -1,5 +1,8 @@
 import { html, css, LitElement } from "lit";
 import { customElement, property } from "lit/decorators.js";
+import { ButtonType } from "../models";
+
+import "@phosphor-icons/webcomponents";
 
 /*
 Font icon support for:
@@ -11,8 +14,11 @@ Add font files to html to enable the functionality.
 */
 @customElement("architecture-icon")
 export class ArchitectureIcon extends LitElement {
-    @property() 
+    @property()
     icon!: string | undefined;
+
+    @property()
+    buttonType!: ButtonType;
 
     override async connectedCallback() {
         super.connectedCallback();
@@ -24,7 +30,19 @@ export class ArchitectureIcon extends LitElement {
         }
 
         if(this.icon.startsWith("ph-")) {
-            return html`<i .className=${this.icon}></i>`;
+            const IconTag = customElements.get(this.icon);
+            if (IconTag) {
+                const iconElement = new IconTag();
+                if (this.buttonType === "default") {
+                    iconElement.setAttribute("color", "var(--color-brand-base)");
+                } else {
+                    iconElement.setAttribute("color", "white");
+                }
+                iconElement.setAttribute("weight", "bold");
+                iconElement.setAttribute("size", "100%");
+                iconElement.setAttribute("style", "display: grid; place-content: center;");
+                return html`${iconElement}`;
+            }
         }
 
         if(this.icon.startsWith("fa-")) {
@@ -35,7 +53,7 @@ export class ArchitectureIcon extends LitElement {
             return html`<span class="material-symbols-outlined">${this.icon.substring(4)}</span>`;
         }
 
-        return html`<img .src=${this.icon} />`
+        return html`<img .src=${this.icon} />` // This will always have the SVG color
     }
 
     static override get styles() {
@@ -46,11 +64,7 @@ export class ArchitectureIcon extends LitElement {
                     align-items: center;
                     justify-content: center;
                     color: var(--color-white);
-                }
-                svg {
-                    width: 100%;
-                    height: 100%;
-                    color: inherit;
+                    overflow: hidden;
                 }
             `,
         ];
