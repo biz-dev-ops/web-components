30a126d5561c14a2ab21c28e2f350419a175706a
Arjan Geertsema <arjan@synion.nl>
2024-02-07 13:03:42 +0100

    removed libraries

--- src/shared/icon/index.ts
+++ src/shared/icon/index.ts
@@ -1,14 +1,7 @@
 import { html, LitElement } from "lit";
-import { unsafeHTML } from "lit/directives/unsafe-html.js";
 import { customElement, property } from "lit/decorators.js";
-import "@phosphor-icons/webcomponents";
-
-import { library, findIconDefinition, icon, IconName } from '@fortawesome/fontawesome-svg-core'
-import { far } from '@fortawesome/free-regular-svg-icons'
-library.add(far)
 
 import 'material-symbols/outlined.css';
-
 import iconCss from './icon.css';
 
 @customElement("bdo-icon")
@@ -21,25 +14,8 @@ export class Icon extends LitElement {
             return html``;
         }
 
-        if(this.icon.startsWith("ph-")) {
-            const IconTag = customElements.get(this.icon);
-            if (IconTag) {
-                const iconElement = new IconTag();
-                iconElement.classList.add("bdo-icon__phosphor");
-                iconElement.setAttribute("size", "100%");
-                return html`${iconElement}`;
-            }
-        }
-
-        if(this.icon.startsWith("fa-")) {
-            const iconName = this.icon.substring(3) as IconName; // remove the "fa-" prefix
-            const iconDefinition = findIconDefinition({ prefix: 'far', iconName: iconName });
-            const svgElement = icon(iconDefinition).html.join('');
-            return html`${unsafeHTML(svgElement)}`;
-        }
-
         if(this.icon.startsWith("mat-")) {
-            return html`<span class="material-symbols-outlined">${this.icon.substring(4)}</span>`;
+            return html`<span class="material-symbols">${this.icon.substring(4)}</span>`;
         }
 
         return html`<img .src=${this.icon} />` // This will always have the SVG color
