e6a3a23eb037b99b074852bd24dd85979f05db24
Jesse van Muijden <jessevanmuijden@gmail.com>
2024-06-10 00:56:07 -0400

    responsive layout

--- src/business-model-canvas/index.ts
+++ src/business-model-canvas/index.ts
@@ -33,6 +33,13 @@ export class BusinessModelCanvasComponent extends LitElement {
       </div>
       <div class="canvas-grid__item">
         <canvas-box
+          .items=${this.model.keyResources || data.keyResources.defaultItems}
+          .icon=${data.keyResources.icon}
+          .title=${data.keyResources.title}
+        />
+      </div>
+      <div class="canvas-grid__item">
+        <canvas-box
           .items=${this.model.valuePropositions ||
           data.valuePropositions.defaultItems}
           .icon=${data.valuePropositions.icon}
@@ -53,13 +60,6 @@ export class BusinessModelCanvasComponent extends LitElement {
           data.customerSegments.defaultItems}
           .icon=${data.customerSegments.icon}
           .title=${data.customerSegments.title}
-        />
-      </div>
-      <div class="canvas-grid__item">
-        <canvas-box
-          .items=${this.model.keyResources || data.keyResources.defaultItems}
-          .icon=${data.keyResources.icon}
-          .title=${data.keyResources.title}
         />
       </div>
       <div class="canvas-grid__item">
@@ -101,30 +101,77 @@ export class BusinessModelCanvasComponent extends LitElement {
       css`
         .canvas-grid {
           width: 100%;
-          background: pink;
           display: grid;
           grid-gap: var(--line-base);
           background-color: var(--color-brand-a40);
-          grid-template-columns: repeat(10, 1fr);
+          grid-template-columns: repeat(1, 1fr);
           border: var(--line-base) solid var(--color-brand-a40);
           margin-top: var(--space-md);
         }
 
         .canvas-grid__item {
-          grid-column: auto / span 2;
           background-color: #ffffff;
           padding: var(--space-xs);
         }
 
-        .canvas-grid__item:nth-child(1),
-        .canvas-grid__item:nth-child(3),
-        .canvas-grid__item:nth-child(5) {
-          grid-row: auto / span 2;
+        @media (min-width: 768px) {
+          .canvas-grid {
+            grid-template-columns: repeat(2, 1fr);
+          }
+
+          .canvas-grid__item:nth-child(3) {
+            grid-column: auto / span 2;
+          }
         }
 
-        .canvas-grid__item:nth-child(8),
-        .canvas-grid__item:nth-child(9) {
-          grid-column: auto / span 5;
+        @media (min-width: 1200px) {
+          .canvas-grid {
+            grid-template-columns: repeat(6, 1fr);
+          }
+
+          .canvas-grid__item {
+            grid-column: auto / span 2;
+          }
+        }
+
+        @media (min-width: 1400px) {
+          .canvas-grid {
+            grid-template-columns: repeat(10, 1fr);
+          }
+
+          .canvas-grid__item {
+            grid-column: auto / span 2;
+            background-color: #ffffff;
+            padding: var(--space-xs);
+          }
+
+          .canvas-grid__item:nth-child(3) {
+            grid-column-start: 3;
+          }
+
+          .canvas-grid__item:nth-child(4) {
+            grid-row: 1 / span 2;
+            grid-column-start: 5;
+          }
+
+          .canvas-grid__item:nth-child(5) {
+            grid-row-start: 1;
+            grid-column-start: 7;
+          }
+
+          .canvas-grid__item:nth-child(6) {
+            grid-row: 1 / span 2;
+            grid-column-start: 9;
+          }
+
+          .canvas-grid__item:nth-child(1) {
+            grid-row: auto / span 2;
+          }
+
+          .canvas-grid__item:nth-child(8),
+          .canvas-grid__item:nth-child(9) {
+            grid-column: auto / span 5;
+          }
         }
       `,
     ];
