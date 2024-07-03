3986f31c7cbe6f59c8a2cc067bfbc45dd45cfba7
Jesse van Muijden <jessevanmuijden@gmail.com>
2024-07-02 20:29:48 -0400

    feat(bpmn): add zoom methods

--- globals.d.ts
+++ globals.d.ts
@@ -1,1 +1,4 @@
-declare module '*.css';
+declare module "*.css";
+
+declare module "bpmn-js-token-simulation";
+declare module "bpmn-js-token-simulation/lib/viewer";
--- src/bpmn-viewer/index.ts
+++ src/bpmn-viewer/index.ts
@@ -1,47 +1,62 @@
-import { css, LitElement, unsafeCSS } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { css, LitElement, unsafeCSS } from "lit";
+import { customElement, property } from "lit/decorators.js";
 import styles from "../shared/styles/reset";
 
-import Viewer from 'bpmn-js/lib/NavigatedViewer'; 
-import ViewerDiagramJsCss from 'bpmn-js/dist/assets/diagram-js.css';
-import ViewerBpmnJsCss from 'bpmn-js/dist/assets/bpmn-js.css';
+import Viewer from "bpmn-js/lib/NavigatedViewer";
+import ViewerDiagramJsCss from "bpmn-js/dist/assets/diagram-js.css";
+import ViewerBpmnJsCss from "bpmn-js/dist/assets/bpmn-js.css";
+import TokenSimulationModule from "bpmn-js-token-simulation/lib/viewer";
+import "bpmn-js-token-simulation/assets/css/bpmn-js-token-simulation.css";
 
-@customElement('bpmn-viewer')
+@customElement("bpmn-viewer")
 export class BPMNViewer extends LitElement {
-  private _viewer!: Viewer;
+  private _viewer!: any;
 
   @property({ attribute: "data-xml" })
-  xml!: string
+  xml!: string;
 
   override async firstUpdated() {
     this._viewer = new Viewer({
-      container: this.renderRoot as HTMLElement
+      container: this.renderRoot as HTMLElement,
+      additionalModules: [TokenSimulationModule],
     });
 
     try {
       const { warnings } = await this._viewer.importXML(this.xml);
 
       if (warnings.length) {
-        console.log('import with warnings', warnings);
+        console.log("import with warnings", warnings);
+      } else {
+        console.log("import successful");
       }
-      else {
-        console.log('import successful');
-      }
-      
-      this._viewer
-        .get('canvas')
-        .zoom('fit-viewport');
-    }
-    catch (err) {
-      console.log('something went wrong:', err);
+
+      this.zoomReset();
+    } catch (err) {
+      console.log("something went wrong:", err);
     }
   }
 
   static override get styles() {
     return [
       styles,
-      css`${unsafeCSS(ViewerDiagramJsCss)}`, 
-      css`${unsafeCSS(ViewerBpmnJsCss) }`
+      css`
+        ${unsafeCSS(ViewerDiagramJsCss)}
+      `,
+      css`
+        ${unsafeCSS(ViewerBpmnJsCss)}
+      `,
     ];
+  }
+
+  public zoomIn() {
+    (this._viewer.get("zoomScroll") as any).stepZoom(1);
+  }
+
+  public zoomOut() {
+    (this._viewer.get("zoomScroll") as any).stepZoom(-1);
+  }
+
+  public zoomReset() {
+    this._viewer.get("canvas").zoom("fit-viewport");
   }
 }
