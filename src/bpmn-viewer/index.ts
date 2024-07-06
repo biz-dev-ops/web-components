57002f7612ff20cbe4fe17cbaf64d841c59ce611
Jesse van Muijden <jessevanmuijden@gmail.com>
2024-07-06 06:04:38 -0400

    fix build

--- src/bpmn-viewer/index.ts
+++ src/bpmn-viewer/index.ts
@@ -1,47 +1,47 @@
-import { css, LitElement, unsafeCSS } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { css, LitElement, unsafeCSS } from "lit";
+import { customElement, property } from "lit/decorators.js";
 import styles from "../shared/styles/reset";
 
-import Viewer from 'bpmn-js/lib/NavigatedViewer'; 
-import ViewerDiagramJsCss from 'bpmn-js/dist/assets/diagram-js.css';
-import ViewerBpmnJsCss from 'bpmn-js/dist/assets/bpmn-js.css';
+import Viewer from "bpmn-js/lib/NavigatedViewer";
+import ViewerDiagramJsCss from "bpmn-js/dist/assets/diagram-js.css";
+import ViewerBpmnJsCss from "bpmn-js/dist/assets/bpmn-js.css";
 
-@customElement('bpmn-viewer')
+@customElement("bpmn-viewer")
 export class BPMNViewer extends LitElement {
-  private _viewer!: Viewer;
+  private _viewer!: any;
 
   @property({ attribute: "data-xml" })
-  xml!: string
+  xml!: string;
 
   override async firstUpdated() {
     this._viewer = new Viewer({
-      container: this.renderRoot as HTMLElement
+      container: this.renderRoot as HTMLElement,
     });
 
     try {
       const { warnings } = await this._viewer.importXML(this.xml);
 
       if (warnings.length) {
-        console.log('import with warnings', warnings);
+        console.log("import with warnings", warnings);
+      } else {
+        console.log("import successful");
       }
-      else {
-        console.log('import successful');
-      }
-      
-      this._viewer
-        .get('canvas')
-        .zoom('fit-viewport');
-    }
-    catch (err) {
-      console.log('something went wrong:', err);
+
+      this._viewer.get("canvas").zoom("fit-viewport");
+    } catch (err) {
+      console.log("something went wrong:", err);
     }
   }
 
   static override get styles() {
     return [
       styles,
-      css`${unsafeCSS(ViewerDiagramJsCss)}`, 
-      css`${unsafeCSS(ViewerBpmnJsCss) }`
+      css`
+        ${unsafeCSS(ViewerDiagramJsCss)}
+      `,
+      css`
+        ${unsafeCSS(ViewerBpmnJsCss)}
+      `,
     ];
   }
 }
