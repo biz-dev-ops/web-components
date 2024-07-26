2753ba34b57ed7f6799c708643241d0a3755765b
Jesse van Muijden <jessevanmuijden@gmail.com>
2024-07-26 12:23:06 -0400

    do not import dev styles during production build

--- src/business-reference-architecture/index.ts
+++ src/business-reference-architecture/index.ts
@@ -4,8 +4,10 @@ import { customElement, property } from "lit/decorators.js";
 import resetStyles from "../shared/styles/reset";
 import { Section } from "./models";
 
-import "@biz-dev-ops/md-docs/assets/style/page/style.css";
-import "../../assets/style/custom-theme.css";
+if (process.env.NODE_ENV !== "production") {
+  require("@biz-dev-ops/md-docs/assets/style/page/style.css");
+  require("../../assets/style/custom-theme.css");
+}
 
 import "./architecture-section";
 
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-import "@biz-dev-ops/md-docs/assets/style/page/style.css";
-import "../assets/style/custom-theme.css";
+if (process.env.NODE_ENV !== "production") {
+  require("@biz-dev-ops/md-docs/assets/style/page/style.css");
+  require("../assets/style/custom-theme.css");
+}
