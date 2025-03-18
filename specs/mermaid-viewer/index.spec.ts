adee1056804872db4adcbfa7fa5be11c29ebe80f
Arjan Geertsema <arjan@synion.nl>
2025-03-18 19:07:16 +0100

    added mermaid viewer

---
+++ specs/mermaid-viewer/index.spec.ts
@@ -0,0 +1,55 @@
+import { test, expect } from "@sand4rt/experimental-ct-web";
+import { MermaidViewer } from "../../src/mermaid-viewer";
+import { FileRoute, useRoutes } from "../helper/router-helper";
+
+test.describe("mermaid-viewer", async () => {
+
+    test.beforeEach(async ({ router }) => {
+        await useRoutes(router, [
+            new FileRoute("/diagram1.mmd", new URL("diagram1.mmd", import.meta.url)),
+            new FileRoute("/diagram2.mmd", new URL("diagram2.mmd", import.meta.url))
+        ]);
+    });
+
+    test("can load src", async ({ mount }) => {
+        const component = await mount(MermaidViewer, {
+            props: {
+                src: "diagram1.mmd"
+            }
+        });
+
+        await expect(component.locator("svg")).toBeVisible();
+    });
+
+    test("can change src", async ({ mount }) => {
+        const component = await mount(MermaidViewer, {
+            props: {
+                src: "diagram1.mmd"
+            }
+        });
+
+        await expect(component.locator("svg")).toBeVisible();
+
+        await component.update({
+            props: {
+                src: "diagram2.mmd"
+            }
+        });
+
+        await expect(component.locator("svg")).toBeVisible();
+    });
+
+    test("shows error for invalid diagram", async ({ mount }) => {
+        const component = await mount(MermaidViewer, {
+            props: {
+                mmd: "invalid mermaid syntax"
+            }
+        });
+
+        await expect(component.locator(".error")).toBeVisible();
+    });
+
+    test.afterEach(async ({ page }) => {
+        await page.close();
+    });
+}); 
---
+++ src/mermaid-viewer/index.ts
@@ -0,0 +1,83 @@
+import { css, html, LitElement } from "lit";
+import { customElement, property, state } from "lit/decorators.js";
+import { unsafeHTML } from "lit/directives/unsafe-html.js";
+
+import resetCss from "../shared/styles/reset.css";
+import themeCss from "../shared/styles/theme.css";
+import { fetchText } from "../shared/fetch";
+
+import mermaid from "mermaid";
+
+export const tag: string = "mermaid-viewer";
+
+@customElement(tag)
+export class MermaidViewer extends LitElement {
+
+    @property({ attribute: "src" })
+    src!: string;
+
+    @property({ attribute: "data-mmd" })
+    mmd!: string;
+
+    @state()
+    state!: string | Error;
+
+    override render() {
+        if (this.state instanceof Error) {
+            return html`<div class="error">${this.state.message}</div>`;
+        }
+
+        return this.state ? html`${unsafeHTML(this.state)}` : html``;
+    }
+
+    override async firstUpdated() {
+        mermaid.initialize({
+            startOnLoad: true,
+            theme: "default",
+            securityLevel: "loose",
+        });
+    }
+
+    override async updated(changedProperties) {
+        if (changedProperties.has("src")) {
+            try {
+                this.mmd = await fetchText(this.src);
+            }
+            catch (error: any) {
+                this.state = error;
+            }
+        }
+
+        if (changedProperties.has("mmd")) {
+            this.state = await this._renderDiagram();
+        }
+    }
+
+    private async _renderDiagram() : Promise<string | Error> {
+        try {
+            const { svg } = await mermaid.render("mermaid-diagram", this.mmd);
+            return svg;
+        }
+        catch (err) {
+           return err as Error;
+        }
+    }
+
+    static override get styles() {
+        return [
+            resetCss,
+            themeCss,
+            css`
+        :host {
+          display: block;
+          width: 100%;
+          height: 100%;
+        }
+        svg {
+          width: 100%;
+          height: 100%;
+        }
+      `
+        ];
+    }
+}
