f7642a274541c1293d3c0b0b20b9c7ab5215d353
Arjan Geertsema <arjan@synion.nl>
2025-03-26 14:55:15 +0100

    wop

--- src/business-reference-architecture/architecture-button/index.ts
+++ src/business-reference-architecture/architecture-button/index.ts
@@ -19,12 +19,12 @@ export class ArchitectureButton extends LitElement {
         switch(this.buttonType) {
             case "brand":
                 return html`<default-architecture-button .button=${this.button} />`;
-            case "stream": 
+            case "stream":
                 return html`<arrow-architecture-button .button=${this.button} />`;
             case "default":
                 return html`<inverted-architecture-button .button=${this.button} />`;
             default:
                 return html`<icon-circle-architecture-button .button=${this.button} />`;
-        }   
+        }
     }
 }
