f7854a5a31d4f43a2969817b1c2d4570d37b956e
Arjan Geertsema <arjan@synion.nl>
2025-03-27 19:05:12 +0100

    icon property verwijderd

--- src/shared/alert/index.ts
+++ src/shared/alert/index.ts
@@ -1,40 +1,36 @@
-import { LitElement, html } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { LitElement, html } from "lit";
+import { customElement, property } from "lit/decorators.js";
 
-import resetCss from '../styles/reset.css';
-import alertCss from './alert.css';
+import resetCss from "../styles/reset.css";
+import alertCss from "./alert.css";
 
 import "../icon";
 
-@customElement('bdo-alert')
+@customElement("bdo-alert")
 export class BdoAlert extends LitElement {
     @property({ type: String })
-    type: 'info' | 'warning' | 'error' = 'info';
-
-    @property({ type: String })
-    icon!: string;
+    type: "info" | "warning" | "error" = "info";
 
     override render() {
-        switch (this.type) {
-            case 'info':
-                this.icon = 'mat-info';
-                break;
-            case 'warning':
-                this.icon = 'mat-warning';
-                break;
-            case 'error':
-                this.icon = 'mat-error';
-                break;
-        }
-
         return html`
             <div class="alert ${this.type ? `alert--${this.type}` : ``}" role="alert">
-                ${this.icon ? html`<bdo-icon icon="${this.icon}" class="alert__icon"></bdo-icon>` : ``}
+                <bdo-icon icon="${getIcon(this.type)}" class="alert__icon"></bdo-icon>
                 <div class="alert__message">
                     <slot></slot>
                 </div>
             </div>
         `;
+
+        function getIcon(type: string) : string {
+            switch (type) {
+                case "warning":
+                    return "mat-warning";
+                case "error":
+                    return "mat-error";
+                default:
+                    return "mat-info";
+            }
+        }
     }
 
     static override get styles() {
