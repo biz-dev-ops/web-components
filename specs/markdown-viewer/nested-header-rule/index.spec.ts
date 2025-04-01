5d05d5efca81fa529cc5d772cff0b0fd1eee050b
Arjan Geertsema <arjan@synion.nl>
2025-04-01 11:29:22 +0200

    added aria expanded tets

--- specs/markdown-viewer/nested-header-rule/index.spec.ts
+++ specs/markdown-viewer/nested-header-rule/index.spec.ts
@@ -16,7 +16,7 @@ test.describe("nestedHeadersRulePlugin", () => {
         expectHtml(markdown, expectedHtml);
     });
 
-    test("should render heading containers and adjust heading tokens", () => {
+    test("should render heading containers", () => {
         const markdown = `
 # Level 1
 
@@ -52,6 +52,53 @@ Level 3 content
 </bdo-heading-container>`
 
         expectHtml(markdown, expectedHtml);
+    });
+
+    test("should render heading containers and set aria-expanded", () => {
+        const markdown = `
+# Level 1
+
+Level 1 content
+
+## Level 2
+
+Level 2 content
+
+### Level 3
+
+Level 3 content
+`;
+
+        const expectedHtml = `
+<bdo-heading-container>
+    <h1 slot="header">Level 1</h1>
+    <p>
+        Level 1 content
+    </p>
+    <bdo-heading-container aria-expanded="true">
+        <h2 slot="header">Level 2</h2>
+        <p>
+            Level 2 content
+        </p>
+        <bdo-heading-container aria-expanded="false">
+            <h3 slot="header">Level 3</h3>
+            <p>
+                Level 3 content
+            </p>
+        </bdo-heading-container>
+    </bdo-heading-container>
+</bdo-heading-container>`
+
+        expectHtml(markdown, expectedHtml, (level: number) => {
+            switch(level) {
+                case 2:
+                    return true;
+                case 3:
+                    return false;
+                default:
+                    return undefined;
+            }
+        });
     });
 
     test("should handle no headers", () => {
