8a2bc549bd633495a30f0a420233fc47af9211e7
Sander Tiekstra <sandertiekstra@gmail.com>
2025-05-16 14:34:50 +0200

    Added fullscreen toggle

--- src/shared/driver/driver.css.ts
+++ src/shared/driver/driver.css.ts
@@ -1,8 +1,31 @@
 import { css } from "lit";
 
 export default css`
+    :host {
+        display: block;   
+        background-color: var(--surface-current)
+    }
+
+    :host(.fullscreen) {
+        background-color: var(--surface-main);
+    }
+    
     .driver {
         display: flex;
         gap: var(--space-xs);
+    }
+
+    :host(.fullscreen) .driver {
+        position: absolute;
+        inset-inline-end: var(--space-sm);
+        inset-block-start: var(--space-sm);
+        z-index: 1;
+    }
+
+    :host(.fullscreen) .driven {
+        height: 100%;
+        inset: 0;
+        position: absolute;
+        width: 100%;
     }
 `;
